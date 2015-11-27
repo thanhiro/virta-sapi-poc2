163f3adb824cc95b016ff02e36134e40b62a11fe
Timo Hanhirova <timo.hanhirova@arcusys.fi>
2015-11-27 09:01:36 +0200

    Add example of programmatic DS definition, pooling missing

--- src/main/java/fi/csc/virta/julkaisut/Main.java
+++ src/main/java/fi/csc/virta/julkaisut/Main.java
@@ -1,54 +1,76 @@
 package fi.csc.virta.julkaisut;
 
 import javax.servlet.ServletException;
+import javax.sql.DataSource;
 
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.Context;
-import org.glassfish.jersey.server.ResourceConfig;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.glassfish.jersey.servlet.ServletContainer;
+
 import java.io.File;
 import java.net.MalformedURLException;
 
 /**
- * Main class.
- *
+ * Main bootstrap of application.
  */
 public class Main {
-  public static void main(String[] args) throws Exception {
-      start();
-  }
+    public static void main(String[] args) throws Exception {
+        start();
+    }
 
-  public static Tomcat start() throws ServletException, LifecycleException,
-          MalformedURLException {
+    public static Tomcat start() throws ServletException, LifecycleException,
+            MalformedURLException {
 
-      // Define a folder to hold web application contents.
-      String webappDirLocation = "src/main/webapp/";
-      Tomcat tomcat = new Tomcat();
+        // Define a folder to hold web application contents.
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
 
-      // Define port number for the web application
-      String webPort = System.getenv("PORT");
-      if (webPort == null || webPort.isEmpty()) {
-          webPort = "8080";
-      }
-      // Bind the port to Tomcat server
-      tomcat.setPort(Integer.valueOf(webPort));
+        tomcat.enableNaming();
 
-      // Define a web application context.
-      Context context = tomcat.addWebapp("/", new File(
-              webappDirLocation).getAbsolutePath());
+        // Define port number for the web application
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        // Bind the port to Tomcat server
+        tomcat.setPort(Integer.valueOf(webPort));
 
-     // Add servlet that will register Jersey REST resources
-      Tomcat.addServlet(context, "jersey-container-servlet", resourceConfig());
-      context.addServletMapping("/*", "jersey-container-servlet");
+        // Define a web application context.
+        Context context = tomcat.addWebapp("/", new File(
+                webappDirLocation).getAbsolutePath());
 
-      tomcat.start();
-      tomcat.getServer().await();
+        context.getNamingResources().addResource(dataSourceResource());
 
-      return tomcat;
-  }
+        // Add servlet that will register Jersey REST resources
+        Tomcat.addServlet(context, "jersey-servlet", resourceConfig());
+        context.addServletMapping("/*", "jersey-servlet");
 
-  private static ServletContainer resourceConfig() {
-      return new ServletContainer(new App());
-  }
+        tomcat.start();
+        tomcat.getServer().await();
+
+        return tomcat;
+    }
+
+    /**
+     * Defining DataSource programmatically
+     *
+     * @return
+     */
+    private static ContextResource dataSourceResource() {
+        ContextResource resource = new ContextResource();
+        resource.setName("jdbc/mssqlDS");
+        resource.setType(DataSource.class.getName());
+        resource.setProperty("driverClassName",
+                "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        resource.setProperty("url", "jdbc:sqlserver://<ip>/<db>");
+        resource.setProperty("username", "<username>");
+        resource.setProperty("password", "<password>");
+        return resource;
+    }
+
+    private static ServletContainer resourceConfig() {
+        return new ServletContainer(new App());
+    }
 }
